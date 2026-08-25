08ab2e42fcad442c483d9e7b59e953e0868d70da
Esteban Ramirez <estebanramirezcc@gmail.com>
2026-08-24 20:42:26 -0500

    Removed unused controller and service

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { IdentityModule } from '../identity/identity.module';
 import { TicketsModule } from '../tickets/tickets.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [IdentityModule, TicketsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- apps/api-gateway/src/app/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getData(): { service: string; status: string } {
-    return { service: 'api-gateway', status: 'ok' };
-  }
-}
